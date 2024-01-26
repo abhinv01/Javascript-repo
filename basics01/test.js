f47b62fd30da43b981103d97eda9bbba4e53d324
Abhinav <abhinavbabar001gmail.com>
2024-01-26 16:29:10 +0530

    basic notes

--- basics01/test.js
+++ basics01/test.js
@@ -1,1 +1,28 @@
 console.log("Nirlon")
+
+// "use strict"; treats all js code as a newer version as per newer standards
+// Node and deno is used to compile js code outside browser
+// documentation on MDN by mozilla or tc39.es by ecmascript
+
+/* Datatypes (primitive)
+    Number, String , Boolean, Null , undefined , Symbol, NaN, BigInt
+    typeof (null, array, object) is object*/
+
+/*pre and post increment 
+    let a = ++9 -> a = 10
+    let a = 9++ -> a = 10 */
+
+/* operations
+    "1"+2 -> "12"
+    1+"2" -> "12"
+    1+2+"3" -> "33" */
+
+/* comparing diff data types
+    null>0 -> false
+    null == 0 -> false
+    null >= 0 -> true
+
+    undefined == 0 false
+    undefined > 0 false
+    undefined < 0 false
+    */
